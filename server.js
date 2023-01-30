a51501ac4e368ee4fde94ca6c48a898b2837a25b
Bela Hamid <belahamid@gmail.com>
2023-01-30 18:19:08 +0000

    connect image endpoint to db

--- server.js
+++ server.js
@@ -106,17 +106,20 @@ app.get("/profile/:id", (req, res) => {
 
 app.put("/image", (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(400).json("not found");
-  }
+  db.from("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json("unable to get entries"));
+
+  //   let found = false;
+
+  //   if (!found) {
+  //     res.status(400).json("not found");
+  //   }
 });
 
 bcrypt.hash("bacon", null, null, function (err, hash) {
