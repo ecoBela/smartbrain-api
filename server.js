b5532790ed37ec7dafd14a276e48e2a203660ead
Bela Hamid <belahamid@gmail.com>
2023-02-16 11:44:33 +0000

    move all request handling functions to controller section

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const knex = require("knex");
 
 const register = require("./controllers/register");
 const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 const db = knex({
   client: "pg",
@@ -25,51 +27,18 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Success");
 });
-
 app.post("/signin", (req, res) => {
   signin.handleSignin(req, res, db, bcrypt);
 });
-
 app.post("/register", (req, res) => {
   register.handleRegister(req, res, db, bcrypt);
 });
-
 app.get("/profile/:id", (req, res) => {
-  const { id } = req.params;
-
-  db.select("*")
-    .from("users")
-    .where({ id: id })
-    .then((user) => {
-      if (user.length) {
-        res.json(user[0]);
-      } else {
-        res.status(400).json("not found");
-      }
-    })
-    .catch((err) => {
-      res.status(400).json("error getting user");
-    });
+  profile.handleProfileGet(req, res, db);
 });
-
 app.put("/image", (req, res) => {
-  const { id } = req.body;
-  db.from("users")
-    .where("id", "=", id)
-    .increment("entries", 1)
-    .returning("entries")
-    .then((entries) => {
-      res.json(entries[0].entries);
-    })
-    .catch((err) => res.status(400).json("unable to get entries"));
-
-  //   let found = false;
-
-  //   if (!found) {
-  //     res.status(400).json("not found");
-  //   }
+  image.handleImage(req, res, db);
 });
-
 app.listen(3001, () => {
   console.log("App running on port 3001");
 });
